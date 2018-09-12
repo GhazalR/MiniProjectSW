33c8b295f5d56762c0c4406cd38f1e0d23804df3
Biagio DeSimone <bdesimone@dhcp-wifi-8021x-155-41-109-248.bu.edu>
2018-09-11 21:20:55 -0400

    added class

--- plotting.py
+++ plotting.py
@@ -4,13 +4,41 @@ import matplotlib.pyplot as plt
 humiditylist = [0,0,0]
 templist = [60,60,60]
 
+class house:
+    owner = ""
+    ID = ""
+    temp = 0
+    hum = 0
+    temp_hist = []
+    hum_hist = []
+    def append_temp(t):
+        temp_hist.append(t)
+    def append_hum(h):
+        hum_hist.append(h)
+
+Home1 = house()
+Home2 = house()
+Home3 = house()
+
+Home1.owner = "Biagio"
+Home2.owner = "Ghazal"
+
+Home1.temp = random.uniform(50,80)
+Home2.temp = random.uniform(50,80)
+
+print(Home1.owner)
+print(Home2.owner)
+print(Home1.temp)
+print(Home2.temp)
+
+
 #if temp is below 60, go up to 75
 #if temp is above 75, scale down to 60
 counter = 0
 
 #makes sure only incrementing one of the three temperatures
 
-for x in range(0,9):
+f""" or x in range(0,9):
     print(counter)
     if templist[counter] <=75:
         templist[counter] +=1
@@ -26,4 +54,4 @@ plt.ylabel('temperature')
 
 #used to get graph to show, click enter to get rid of it 
 plt.show(block=False)
-input('press <ENTER> to continue')
+input('press <ENTER> to continue') """
