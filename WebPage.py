ca63a1d7fdee42aafbc5e88e1e78680dde1d563e
Biagio DeSimone <bdesimone@dhcp-wifi-8021x-155-41-75-105.bu.edu>
2018-09-19 22:12:04 -0400

    Additional Sensor Button

--- WebPage.py
+++ WebPage.py
@@ -40,16 +40,6 @@ def main():
                                       }
                                     });
                                 });
-                                $('#btnSend3').click(function(){
-
-                                    $.ajax({
-                                      type: 'POST3',
-                                      url: '/process',
-                                      success: function(data){
-                                        alert(data);
-                                      }
-                                    });
-                                });
                            });
                       </script>
                    </head>
@@ -58,7 +48,7 @@ def main():
                     <h3 style="text-align:center;"> By Biagio DeSimone & Ghazal Randhawa </h3> 
                     <div style="text-align:center;">
                         <input type="button" id="btnSend" value="Show Temperature and Humidity Graphs">
-                        <input type="button" id="btnSend2" value="Add Sensor">    
+                        <input type="button" id="btnSend2" value="Add Additional Temperature And Humidity Sensor">    
                     </div>                
                     </body>
                    </html>"""
@@ -66,58 +56,83 @@ def main():
 
 @app.route('/process', methods=['POST','POST2'])
 def view_do_something():
-    t_sensors = 2
-    h_sensors = 2
-    
-    if request.method == 'POST2': 
-        t_sensors +=1
-        return "OK"
-    if request.method == 'POST3': 
-        h_sensors +=1
-        return "OK"
-    
+
     if request.method == 'POST':
         #your database process here
         humiditylist = [0,0,0,0,0,0,0]
         templist = [60,61,65,64,63,63,62]
-        plotting_temp =[]
-        plotting_hum = []
         counter = 0
         counter2 = 0
-        t_counter = 0 #counter for plotting temp sensors
-        h_counter = 0 #counter for plotting hum sensors
         time_temp = [1,2,3,4,5,6,7]
         time_hum = [1,2,3,4,5,6,7]
         #makes sure only incrementing one of the three temperatures
-        for a in range(0,t_sensors):
-            for y in range(0,len(templist)):
-                if templist[counter] <=75:
-                    templist[counter] +=1
-                else:
-                    templist[counter] -=1
-                if counter <=1:
-                    counter +=1
-                else:
-                    counter = 0
-            plotting_temp[t_counter] = templist
-            t_counter += 1
 
-        for b in range(0,h_sensors):
-            for x in range(0,len(humiditylist)):
-                humiditylist[counter2] = random.randint(20,90)
-                counter2 += 1
-            plotting_hum[h_counter]=humiditylist
-            h_counter += 1
+        for y in range(0,len(templist)):
+            if templist[counter] <=75:
+                templist[counter] +=1
+            else:
+                templist[counter] -=1
+            if counter <=1:
+                counter +=1
+            else:
+                counter = 0
+
+        for x in range(0,len(humiditylist)):
+            humiditylist[counter2] = random.randint(30,90)
+            counter2 += 1
+
 
         plt.figure(1)
         plt.subplot(211)
-        plt.title('Temperature vs Time (in F)')
-        plt.subplot(211).set_ylabel('Temperature')
+        plt.title('Temperature vs Time Sensor 1')
+        plt.subplot(211).set_ylabel('Temperature (F)')
+        #plt.suptitle('Living Room Sensors', fontsize = 16)
         plt.plot(time_temp,templist)
 
         plt.subplot(212)
-        plt.title('Humidity vs Time (%)')
-        plt.subplot(212).set_ylabel('Humidity')
+        plt.title('Humidity % vs Time Sensor 1')
+        plt.subplot(212).set_ylabel('Humidity (%)')
+        plt.plot(time_hum,humiditylist)
+
+        plt.subplots_adjust(left=.125, right = .9,  bottom=.05, top=.9, wspace=.2, hspace=.2)
+
+        mpld3.show()
+        return "OK"
+    if request.method == 'POST2': 
+        #your database process here
+        humiditylist = [0,0,0,0,0,0,0]
+        templist = [60,61,65,64,63,63,62]
+        counter = 0
+        counter2 = 0
+        time_temp = [1,2,3,4,5,6,7]
+        time_hum = [1,2,3,4,5,6,7]
+        #makes sure only incrementing one of the three temperatures
+
+        for y in range(0,len(templist)):
+            if templist[counter] <=75:
+                templist[counter] +=1
+            else:
+                templist[counter] -=1
+            if counter <=1:
+                counter +=1
+            else:
+                counter = 0
+
+        for x in range(0,len(humiditylist)):
+            humiditylist[counter2] = random.randint(30,90)
+            counter2 += 1
+
+
+        plt.figure(2)
+        plt.subplot(221)
+        plt.title('Temperature vs Time Sensor 2')
+        plt.subplot(221).set_ylabel('Temperature (F)')
+        #plt.suptitle('Living Room Sensors', fontsize = 16)
+        plt.plot(time_temp,templist)
+
+        plt.subplot(222)
+        plt.title('Humidity % vs Time Sensor 2')
+        plt.subplot(222).set_ylabel('Humidity (%)')
         plt.plot(time_hum,humiditylist)
 
         plt.subplots_adjust(left=.125, right = .9,  bottom=.05, top=.9, wspace=.2, hspace=.2)
